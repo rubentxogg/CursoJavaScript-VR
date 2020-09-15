ffe1401f356d4dcbeba17e9b4f69cb68d70609fe
Rubén García <rubentxogg@hotmail.com>
2020-09-15 17:02:18 +0200

    Condicionales

---
+++ hola-mundo/js/07-switch.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// Switch
+
+var edad = 40;
+var imprime = "";
+
+switch(edad){
+    case 18: 
+            imprime = "Acabas de cumplir la mayoría de edad";
+            break;
+    case 25:
+            imprime = "Ya eres un adulto";
+            break;
+    case 40:
+            imprime = "Crisis de los 40";
+            break;
+    case 75:
+            imprime = "Eres ya un anciano";
+            break;
+    default:
+            imprime = "Tu edad es neutra";
+            break;
+}
+
+console.log(imprime);
