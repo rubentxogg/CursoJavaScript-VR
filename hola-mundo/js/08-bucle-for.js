57b61279c54865f89ac7ec0ddcf30c526f1efbf3
Rubén García <rubentxogg@hotmail.com>
2020-09-16 12:09:28 +0200

    Iteradores y bucles

---
+++ hola-mundo/js/08-bucle-for.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Bucle For
+// Es una estructura de control que se repite varias veces
+
+var numero = 100;
+
+for(var i=0; i<=numero; i++){
+    console.log("Vamos por el número "+i);
+
+    // debugger;
+}
