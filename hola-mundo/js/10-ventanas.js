38954cbf99ea63cb32cd875c03569fd9ccb66adb
Rubén García <rubentxogg@hotmail.com>
2020-09-16 12:22:28 +0200

    Alertas y ventanas

---
+++ hola-mundo/js/10-ventanas.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//Alerta
+/* alert("Esta es mi alerta!!");
+alert("Este es mi texto"); */
+
+//Confirmación
+/* var mi_resultado = confirm("¿Estás seguro de querer continuar?");
+console.log(mi_resultado); */
+
+// Ingreso datos
+var mi_resultado = prompt("¿Qúe edad tienes?", 18);
+console.log(mi_resultado);
