c538f825198160e9ea6b3726fc6ec266dcfc003a
Rubén García <rubentxogg@hotmail.com>
2020-09-20 13:41:57 +0200

    LocalStorage añadido

---
+++ hola-mundo/js/34-local-storage.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// Localstorage
+
+// Comprobar disponibilidad del Localstorage
+if(typeof(Storage) !== "undefined"){
+    console.log("Localstorage disponible");
+}else{
+    console.log("Incompatible con Localstorage");
+}
+
+// Guardar datos
+localStorage.setItem("titulo", "Curso de Symfony de Víctor Robles");
+
+// Recuperar elemento
+document.querySelector("#peliculas").innerHTML = (localStorage.getItem("titulo"));
+
+// Guardar objetos
+var usuario = {
+    nombre: "Víctor Robles",
+    email: "victor@victor.com",
+    web: "victorroblesweb.es"
+};
+
+localStorage.setItem("usuario", JSON.stringify(usuario));
+
+// Recuperar objeto
+var userjs = JSON.parse(localStorage.getItem("usuario"));
+
+console.log(userjs);
+document.querySelector("#datos").append("\t"+userjs.web+" - "+userjs.nombre);
+
+localStorage.clear();
