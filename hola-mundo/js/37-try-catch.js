7c28a9abae28515615f94600031844d5a25d1356
Rubén García <rubentxogg@hotmail.com>
2020-09-20 18:42:19 +0200

    Peticiones asíncronas

---
+++ hola-mundo/js/37-try-catch.js
@@ -0,0 +1,11 @@
+'use strict'
+
+try {
+    var year = 2019;
+    alert(year);
+    var vector = new Array(999999999999999999999);
+
+} catch (error) {
+    console.log(error);
+    alert("Ha ocurrido un error en el código");
+}
